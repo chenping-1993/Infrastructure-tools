65e91d16104a92107ea940d55ef1e95d1f285950
chenping-1993 <495761000@qq.com>
2021-01-20 13:44:04 +0800

    增加导出PDF文件示例

--- src/main/java/com/example/cp/common/tool/ExcelUtil.java
+++ src/main/java/com/example/cp/common/tool/ExcelUtil.java
@@ -2,14 +2,18 @@ package com.example.cp.common.tool;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.word.WordExportUtil;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description:
@@ -21,13 +25,13 @@ public class ExcelUtil {
     /**
      * 导出excel
      *
-     * @param list 数据集合
+     * @param list      数据集合
      * @param pojoClass 数据类型
-     * @param fileName 文件名称
-     * @param title 表明
-     * @param response 响应对象
+     * @param fileName  文件名称
+     * @param title     表明
+     * @param response  响应对象
      */
-    public static void exportExcel(List<?> list, Class<?> pojoClass, String fileName, String title, HttpServletResponse response,HttpServletRequest request) {
+    public static void exportExcel(List<?> list, Class<?> pojoClass, String fileName, String title, HttpServletResponse response, HttpServletRequest request) {
         ExportParams exportParams = new ExportParams(title, null);
         Workbook workbook = ExcelExportUtil.exportExcel(exportParams, pojoClass, list);
 
@@ -38,9 +42,9 @@ public class ExcelUtil {
         if (workbook != null) {
             try {
                 if (agent.contains("MSIE") || agent.contains("Trident")) {//IE浏览器
-                    fileName = URLEncoder.encode(fileName,"utf-8");
+                    fileName = URLEncoder.encode(fileName, "utf-8");
                 } else {//非IE浏览器
-                    fileName = new String(fileName.getBytes(StandardCharsets.UTF_8),StandardCharsets.ISO_8859_1);
+                    fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
                 }
                 response.setCharacterEncoding("UTF-8");
                 response.setHeader("content-Type", "application/vnd.ms-excel");
@@ -49,6 +53,44 @@ public class ExcelUtil {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    /**
+     * @Description:  导出word文件
+     * @param: filePath 源文件地址
+     * @param: fileName 要导出的文件名
+     * @param: response
+     * @param: request
+     * @return: void
+     * @Author: chenping
+     * @Date: 2021/1/20 11:12
+     */
+    public static void exportWord(String filePath, String fileName, HttpServletResponse response, HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("department", "Easypoi");
+        map.put("person", "JueYue");
+        map.put("me","JueYue");
+        map.put("date", "2015-01-03");
+        try {
+            XWPFDocument doc = WordExportUtil.exportWord07(filePath, map);
+
+            String agent = request.getHeader("User-Agent");
+
+            if (doc != null) {
+
+                if (agent.contains("MSIE") || agent.contains("Trident")) {//IE浏览器
+                    fileName = URLEncoder.encode(fileName, "utf-8");
+                } else {//非IE浏览器
+                    fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+                }
+                response.setCharacterEncoding("UTF-8");
+                response.setHeader("content-Type", "application/vnd.ms-excel");
+                response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+                doc.write(response.getOutputStream());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/main/java/com/example/cp/controller/TestWordController.java
@@ -0,0 +1,78 @@
+package com.example.cp.controller;
+
+
+import cn.afterturn.easypoi.word.WordExportUtil;
+
+import com.example.cp.common.tool.ExcelUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: excel 测试
+ * @Author: chenping
+ * @Date: 2020-11-16
+ */
+@Slf4j
+@Api(tags = "word 测试")
+@RestController
+public class TestWordController {
+
+    /**
+     * @Description:  代码指定word文件导出位置
+     * @param:
+     * @return: void
+     * @Author: chenping
+     * @Date: 2021/1/20 11:13
+     */
+    @ApiOperation(value = "导出word-指定导出位置" ,  notes="导出word-指定导出位置")
+    @GetMapping(value = "/exportWord")
+    public void simpleWordExport() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("department", "Easypoi");
+        map.put("person", "JueYue");
+//        map.put("time", format.format(new Date()));
+        map.put("me","JueYue");
+        map.put("date", "2015-01-03");
+        try {
+            //源文件
+            XWPFDocument doc = WordExportUtil.exportWord07(
+                    "C:/Users/chenping/Desktop/kafka.docx", map);
+            //目标文件
+            FileOutputStream fos = new FileOutputStream("C:/Users/chenping/Desktop/simple.docx");
+            doc.write(fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @Description:  导出word文件  点击下载文件
+     * @param: response
+     * @param: request
+     * @return: void
+     * @Author: chenping
+     * @Date: 2021/1/20 11:13
+     */
+    @ApiOperation(value = "导出word-点击下载" ,  notes="导出word-点击下载")
+    @GetMapping(value = "/exportWord1")
+    public void simpleWordExport1(HttpServletResponse response, HttpServletRequest request) {
+        ExcelUtil.exportWord("C:/Users/chenping/Desktop/kafka.docx","kafka1.docx",response,request);
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
