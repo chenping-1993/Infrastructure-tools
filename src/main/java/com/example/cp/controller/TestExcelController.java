29c906265aa2226a56fb4d5e151d20ce10e0e3c8
chenping-1993 <495761000@qq.com>
2021-02-08 10:38:41 +0800

    增加导出单元格合并excel示例

--- src/main/java/com/example/cp/controller/TestExcelController.java
+++ src/main/java/com/example/cp/controller/TestExcelController.java
@@ -6,6 +6,7 @@ import cn.afterturn.easypoi.excel.ExcelXorHtmlUtil;
 import cn.afterturn.easypoi.excel.entity.ExcelToHtmlParams;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import com.example.cp.common.tool.ExcelUtil;
+import com.example.cp.entity.City;
 import com.example.cp.entity.User;
 import com.example.cp.mapper.UserMapper;
 import com.example.cp.service.UserService;
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,6 +49,14 @@ public class TestExcelController {
     public void exportExcel(HttpServletResponse response, HttpServletRequest request) {
         List<User> list = userMapper.selectAll();
         ExcelUtil.exportExcel(list,User.class,"用户信息表","用户信息统计",response,request);
+    }
+
+    @ApiOperation(value = "导出单元格合并excel" ,  notes="导出单元格合并excel")
+    @GetMapping(value = "/exportMergeExcel")
+    public void exportMergeExcel(HttpServletResponse response, HttpServletRequest request) {
+        List<City> list = new ArrayList<>();
+        formData(list);
+        ExcelUtil.exportExcel(list,City.class,"城市信息","城市信息",response,request);
     }
 
     /*
@@ -85,5 +95,34 @@ public class TestExcelController {
         List<User> list = ExcelImportUtil.importExcel(inputStream, User.class, importParams);
         return list;
     }
+    private static void formData(List<City> list) {
+        City city = new City("菏泽",2345,16,"山东");
+        City city1 = new City("枣庄",2945,11,"山东");
+        City city2 = new City("德州",8345,17,"山东");
+        City city3 = new City("济南",8945,13,"山东");
 
+        City city4 = new City("郑州",4670,18,"河南");
+        City city5 = new City("开封",4550,21,"河南");
+        City city6 = new City("洛阳",4580,27,"河南");
+        City city7 = new City("新乡",4530,23,"河南");
+
+        City city8 = new City("西安",2339,5,"陕西");
+        City city9 = new City("渭南",2724,22,"陕西");
+        City city10 = new City("宝鸡",3671,7,"陕西");
+        City city11 = new City("榆林",4357,24,"陕西");
+        City city12 = new City("榆林",8888,29,"陕西");
+        list.add(city);
+        list.add(city1);
+        list.add(city2);
+        list.add(city3);
+        list.add(city4);
+        list.add(city5);
+        list.add(city6);
+        list.add(city7);
+        list.add(city8);
+        list.add(city9);
+        list.add(city10);
+        list.add(city11);
+        list.add(city12);
+    }
 }
---
+++ src/main/java/com/example/cp/entity/City.java
@@ -0,0 +1,30 @@
+package com.example.cp.entity;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: chenping
+ * @Date: 2021-02-08
+ */
+@Data
+public class City {
+
+    @Excel(name = "省份",mergeVertical = true)//mergeVertical 纵向合并内容相同的单元格 默认false
+    private String province;
+    @Excel(name = "城市")
+    private String cityName;
+    @Excel(name = "人口")
+    private Integer population;
+    @Excel(name = "排名")
+    private Integer rank;
+
+
+    public City(String cityName, Integer population, Integer rank, String province) {
+        this.cityName = cityName;
+        this.population = population;
+        this.rank = rank;
+        this.province = province;
+    }
+}
