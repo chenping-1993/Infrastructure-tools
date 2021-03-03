d4b233910f41ff45e6c0681e4fb443b93ae11c7b
chenping-1993 <495761000@qq.com>
2021-03-03 18:01:29 +0800

    rabbitmq 示例 swagger注解添加

--- src/main/java/com/example/cp/controller/RabbitMqSendController.java
+++ src/main/java/com/example/cp/controller/RabbitMqSendController.java
@@ -1,5 +1,7 @@
 package com.example.cp.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author: chenping
  * @Date: 2021-03-02
  */
+@Api(tags = "RabbitMq 测试")
 @RestController
 public class RabbitMqSendController {
 
@@ -23,6 +26,7 @@ public class RabbitMqSendController {
      * @Author: chenping
      * @Date: 2021/3/2 14:26
      */
+    @ApiOperation(value = "一对一发送" ,  notes="一个消息 一个消息监听接收者")
     @GetMapping("/rabbitmq/send")
     public String oneToOneSend() {
         String message = "server message";
@@ -38,6 +42,7 @@ public class RabbitMqSendController {
      * @Author: chenping
      * @Date: 2021/3/2 14:30
      */
+    @ApiOperation(value = "一对多发送消息" ,  notes="一个队列 多个消息监听者")
     @GetMapping("/rabbitmq/oneToManySend")
     public String oneToManySend() {
         StringBuilder times=new StringBuilder();
@@ -56,6 +61,7 @@ public class RabbitMqSendController {
      * @Author: chenping
      * @Date: 2021/3/2 14:50
      */
+    @ApiOperation(value = "多对多发送消息" ,  notes="多个队列 多个消息接收者")
     @GetMapping("/rabbitmq/manyToManySend")
     public String manyToManySend() {
         StringBuilder times=new StringBuilder();
@@ -76,6 +82,7 @@ public class RabbitMqSendController {
      * @Author: chenping
      * @Date: 2021/3/3 10:42
      */
+    @ApiOperation(value = "发送到其他的系统-服务端和客户端两个系统" ,  notes="发送到其他的系统-服务端和客户端两个系统")
     @GetMapping("/rabbitmq/sendToClient")
     public String sendToClient() {
         String message = "server message sendToClient";
@@ -90,7 +97,7 @@ public class RabbitMqSendController {
      * topic.message 队列发送的消息 能被监听 topic.message 和监听 topic.messages的监听接收者 接收到消息
      * topic.messages 队列发送的消息 只能被监听 topic.messages的监听接收者 接收到消息
      */
-
+    @ApiOperation(value = "topic.message 监听者监听以topic.开头的都能收到消息")
     @GetMapping("/topicSend1")
     public String  topicSend1() {
         String context = "my topic 1";
@@ -98,6 +105,7 @@ public class RabbitMqSendController {
         this.amqpTemplate.convertAndSend("exchange", "topic.message", context);
         return context;
     }
+    @ApiOperation(value = "topic.message 监听者只监听topic.messages的消息")
     @GetMapping("/topicSend2")
     public String topicSend2() {
         String context = "my topic 2";
